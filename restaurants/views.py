02270038ccf5ee387850ca2ba2bf9946360920fe
amal-nj <52827969+amal-nj@users.noreply.github.com>
2019-07-22 19:15:21 +0300

    task 2 done

--- restaurants/views.py
+++ restaurants/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def helloWorld(request):
+    context={
+        "msg": "Hello World!"
+    }
+    return render(request,'task2.html',context)
